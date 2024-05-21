0a7eb58b5bfc128d08f91fbb5356fb8a82ca3369
Manuel Naranjo <125095298+UdadNrj@users.noreply.github.com>
2024-05-21 15:02:46 +0200

    3

--- lib/view/BNavigator/page_settings.dart
+++ lib/view/BNavigator/page_settings.dart
@@ -10,64 +10,155 @@ class PageSetting extends StatefulWidget {
 class _PageSettingState extends State<PageSetting> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(-0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'CONTROL DE ACCESOS',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
+          children: <Widget>[
+            const Card(
+              child: ListTile(
+                title: Text("Manuel Naranjo"),
+                leading: CircleAvatar(),
+                trailing: Icon(Icons.edit),
               ),
             ),
-            const SizedBox(height: 8.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 3,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  List<String> powerTitles = ["Title 1", "Title 2", "Title 3"];
-                  return SwitchItem(
-                    title: powerTitles[index],
-                  );
+            const Card(
+              margin: EdgeInsets.fromLTRB(32.0, 8.0, 32.0, 8.0),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(
+                        Icons.lock_outline_sharp,
+                      ),
+                      title: Text("Change Password"),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.lock_outline_sharp,
+                      ),
+                      title: Text("Change Lenguage"),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Text(
+              "CONTROL DE ACCESOS",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              color: Colors.white,
+              child: SwitchListTile(
+                activeColor: const Color(0xFFCC3366),
+                value: true,
+                title: const Text("Received notification"),
+                onChanged: (bool value) {
+                  // handle onChanged
                 },
               ),
             ),
+            SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: const Text("Received notification"),
+              onChanged: (bool value) {
+                // handle onChanged
+              },
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const Text(
+              "MODO DE LECTURA",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: const Text("Received notification"),
+              onChanged: (bool value) {
+                // handle onChanged
+              },
+            ),
+            const SwitchListTile(
+              activeColor: const Color(0xFFCC3366),
+              value: true,
+              title: Text("Received notification"),
+              onChanged: null,
+            ),
+            const Text(
+              "AYUDA",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.lock_outline_sharp,
+              ),
+              title: Text("Contacto"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.lock_outline_sharp,
+              ),
+              title: Text("Terminos y condiciones"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.lock_outline_sharp,
+              ),
+              title: Text("saber mas"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.lock_outline_sharp,
+              ),
+              title: Text("Legal"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("CERRAR SESION"))
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SwitchItem extends StatefulWidget {
-  final String title;
-  const SwitchItem({super.key, required this.title});
-
-  @override
-  State<SwitchItem> createState() => _SwitchItemState();
-}
-
-class _SwitchItemState extends State<SwitchItem> {
-  bool isSelected = false;
-  void itemSwitch(bool value) {
-    setState(() {
-      isSelected = !isSelected;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text("Power ${widget.title}"),
-      subtitle: Text(isSelected ? "ON" : "OFF"),
-      trailing: Switch(
-        value: isSelected,
-        onChanged: itemSwitch,
-        activeColor: const Color.fromARGB(235, 36, 230, 230),
       ),
     );
   }
